29e1fb7aaad98a4fef027035f54f95bfe5056b0b
osaroadade <adadechuks@gmail.com>
2023-01-08 17:22:33 +0100

    Setup eleventy and main page.

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,13 @@
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addWatchTarget("./src/sass/")
+    eleventyConfig.addWatchTarget("./src/scss/")
+    eleventyConfig.addPassthroughCopy("./src/assets")
+    eleventyConfig.addPassthroughCopy("./src/js")
+    eleventyConfig.addPassthroughCopy("./src/css")
 
-	return {
-		dir: {
-			input: "src",
-			output: "public",
-		}
-	}
+    return {
+        dir: {
+            input: "src",
+            output: "public",
+        }
+    }
 }
